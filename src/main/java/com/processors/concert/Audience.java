9c20b0808904f31f96148ca94c78a2c0173054a8
Rumman Bin Ashraf <rumman.ashraf@gmail.com>
2021-05-18 23:53:23 +0600

    Replaced All Advice with Single @Around Advice

--- src/main/java/com/processors/concert/Audience.java
+++ src/main/java/com/processors/concert/Audience.java
@@ -1,6 +1,9 @@
 package com.processors.concert;
 
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -19,23 +22,32 @@ public class Audience {
     public void performPointCut() {
     }
 
-    @Before("performPointCut()")
     public void silencePhone() {
         logger.debug("Silencing phone of audience.");
     }
 
-    @Before("performPointCut()")
     public void takeSeats() {
         logger.debug("Taking Seats Before Performance.");
     }
 
-    @AfterReturning("performPointCut()")
     public void clap() {
         logger.debug("Clapping after successful performance.");
     }
 
-    @AfterThrowing("performPointCut()")
     public void demandRefund() {
         logger.debug("Demanding Refund for bad performance.");
+    }
+
+    @Around("performPointCut()")
+    public void doAllInSingleAdvice(ProceedingJoinPoint proceedingJoinPoint) {
+        silencePhone();
+        takeSeats();
+
+        try {
+            proceedingJoinPoint.proceed();
+            clap();
+        } catch (Throwable throwable) {
+            demandRefund();
+        }
     }
 }
